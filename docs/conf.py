990bcc4c54045936dddb8315fcc7719265bcc68b
vtog <v.tognaci@f5.com>
2019-04-10 16:24:51 -0500

    cleanup of build scripts and repo, update to latest f5-sphinx-theme

--- docs/conf.py
+++ docs/conf.py
@@ -52,10 +52,12 @@ rst_prolog = """
 .. |f5|  replace:: F5 Networks
 .. |f5i| replace:: F5 Networks, Inc.
 .. |year| replace:: %s
+.. |github_repo| replace:: %s
 """ % (classname,
        classname,
        classname,
-       year)
+       year,
+       github_repo)
 
 if 'github_repo' in locals() and len(github_repo) > 0:
     rst_prolog += """
@@ -73,6 +75,28 @@ on_snops = os.environ.get('SNOPS_ISALIVE', None) == 'True'
 print "on_rtd = %s" % on_rtd
 print "on_snops = %s" % on_snops
 
+branch_map = {
+    "stable":"master",
+    "latest":"master"
+}
+
+try:
+    if not on_rtd:
+        from git import Repo
+        repo = Repo("%s/../" % os.getcwd())
+        git_branch = repo.active_branch
+        git_branch_name = git_branch.name
+    else:
+        git_branch_name = os.environ.get('READTHEDOCS_VERSION', None)
+except:
+    git_branch_name = 'master'
+
+print "guessed git branch: %s" % git_branch_name
+
+if git_branch_name in branch_map:
+    git_branch_name = branch_map[git_branch_name]
+    print " remapped to git branch: %s" % git_branch_name
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -82,14 +106,42 @@ print "on_snops = %s" % on_snops
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
   'sphinx.ext.todo',
+  'sphinx.ext.extlinks',
+  'sphinx.ext.graphviz',
+  'sphinxcontrib.nwdiag',
+  'sphinxcontrib.blockdiag'
   #'sphinx.ext.autosectionlabel'
 ]
 
 if 'googleanalytics_id' in locals() and len(googleanalytics_id) > 0:
   extensions += ['sphinxcontrib.googleanalytics']
   googleanalytics_enabled = True
+
+graphviz_output_format = 'svg'
+graphviz_font = 'DejaVu Sans:style=Book'
+graphviz_dot_args = [
+     "-Gfontname='%s'" % graphviz_font,
+     "-Nfontname='%s'" % graphviz_font,
+     "-Efontname='%s'" % graphviz_font
+]
+
+html_context = {
+  "github_url":github_repo,
+  "github_branch":git_branch_name
+}
+
+diag_fontpath = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
+diag_html_image_format = 'SVG'
+diag_latex_image_format = 'PNG'
+diag_antialias = False
+
+blockdiag_fontpath = nwdiag_fontpath = diag_fontpath
+blockdiag_html_image_format = nwdiag_html_image_format = diag_html_image_format
+blockdiag_latex_image_format = nwdiag_latex_image_format = diag_latex_image_format
+blockdiag_antialias = nwdiag_antialias = diag_antialias
 
 eggs_loader = pkgutil.find_loader('sphinxcontrib.spelling')
 found = eggs_loader is not None
@@ -123,7 +175,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = classname
-copyright = u'2017, F5 Networks, Inc.'
+copyright = u'2019, F5 Networks, Inc.'
 author = u'F5 Networks, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -173,6 +225,10 @@ def setup(app):
 
 if on_rtd:
     templates_path = ['_templates']
+
+extlinks = {
+    'issues':( ("%s/issues/%%s" % github_repo), 'issue ' )
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -256,4 +312,6 @@ texinfo_documents = [
      author, classname, classname,
      'Training'),
 ]
+
+
 
