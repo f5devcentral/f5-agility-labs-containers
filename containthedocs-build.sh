8faf615e74ee4fa55451e2a4921f61f0f740b19f
vtog <v.tognaci@f5.com>
2021-03-25 10:11:52 -0500

    updated build commands

--- containthedocs-build.sh
+++ containthedocs-build.sh
@@ -2,10 +2,12 @@
 
 set -x
 
+COMMAND=(/bin/bash -c "sudo pip install -U sphinx ; make -C docs clean ; make -C docs html")
+
 . ./containthedocs-image
 
 exec docker run --rm -t \
   -v "$PWD":"$PWD" --workdir "$PWD" \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
-  ${DOC_IMG} /bin/bash -c "sudo pip install -U sphinx ; make -C docs clean ; make -C docs html"
+  ${DOC_IMG} "${COMMAND[@]}"
